039b594d15a7c6e926829dc8b4d734124adbfb52
정재훈 <jjh_2708@naver.com>
2016-05-18 21:28:00 +0900

    XML 파싱부분 일부 구현
    XML 파싱부분 일부 구현

--- Toilet Searcher/Toilet_Searcher.py
+++ Toilet Searcher/Toilet_Searcher.py
@@ -13,3 +13,16 @@ from urllib.parse import urlparse
 url="http://openapi.gg.go.kr/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1"
 parts = urlparse(url)
 print(parts)
+
+import xml.parsers.expat
+def start_element(name, attrs):
+    print('Start element:', name, attrs) 
+
+def char_data(data):
+    print('Character data:', repr(data))
+
+pa = xml.parsers.expat.ParserCreate() 	#xmlparser 객체 생성
+pa.StartElementHandler = start_element 	#이벤트 핸들러 연결
+pa.CharacterDataHandler = char_data 	#이벤트 핸들러 연결
+pa.Parse("""<?xml version="1.0"?><book ISBN="1111">
+<title>Loving Python</title></book>""")
