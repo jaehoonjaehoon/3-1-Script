96dd19aeaa9b7471ff75043d3a2bb6a679d940f0
정재훈 <jjh_2708@naver.com>
2016-05-20 21:50:40 +0900

    코드 일부분 변경수정
    코드 일부분 변경수정

--- Toilet Searcher/Toilet_Searcher.py
+++ Toilet Searcher/Toilet_Searcher.py
@@ -1,28 +1,15 @@
-#from tkinter import *
+﻿import http.client
+conn = http.client.HTTPConnection("openapi.gg.go.kr")
+conn.request("GET", "/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&pIndex=1&pSize=50&SIGUN_CD=41310") #서버에 GET 요청 
+req = conn.getresponse() #openAPI 서버에서 보내온 요청을 받아옴
+print(req.status,req.reason)
 
-#app = Tk()
-#app.title("your")
-#app.geometry('800x600')
-
-#b1 = Button(app, text = "click me!", width = 10)
-#b1.pack()
-
-#app.mainloop()
-
-from urllib.parse import urlparse
-url="http://openapi.gg.go.kr/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1"
-parts = urlparse(url)
-print(parts)
-
-import xml.parsers.expat
-def start_element(name, attrs):
-    print('Start element:', name, attrs) 
-
-def char_data(data):
-    print('Character data:', repr(data))
-
-pa = xml.parsers.expat.ParserCreate() 	#xmlparser 객체 생성
-pa.StartElementHandler = start_element 	#이벤트 핸들러 연결
-pa.CharacterDataHandler = char_data 	#이벤트 핸들러 연결
-pa.Parse("""<?xml version="1.0"?><book ISBN="1111">
-<title>Loving Python</title></book>""")
+#import http.client
+#import xml.dom.minidom
+#s=http.client.HTTPConnection("openapi.gg.go.kr")
+#s.request("GET","/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1")
+#req=s.getresponse()
+#data=req.read()
+#dom=xml.dom.minidom.parseString(data)
+#print(req.getheaders())
+#print(dom.toxml())
