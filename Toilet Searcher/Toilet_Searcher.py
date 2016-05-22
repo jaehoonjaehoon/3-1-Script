13450706caee95f52439389426d3b01b084ce3fb
정재훈 <jjh_2708@naver.com>
2016-05-22 17:19:47 +0900

    내용 이해에 대한 코드 작성
    내용 이해에 대한 코드 작성

--- Toilet Searcher/Toilet_Searcher.py
+++ Toilet Searcher/Toilet_Searcher.py
@@ -1,15 +1,15 @@
-﻿import http.client
-conn = http.client.HTTPConnection("openapi.gg.go.kr")
-conn.request("GET", "/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&pIndex=1&pSize=50&SIGUN_CD=41310") #서버에 GET 요청 
-req = conn.getresponse() #openAPI 서버에서 보내온 요청을 받아옴
-print(req.status,req.reason)
+#-*- encoding: utf-8 -*-
 
-#import http.client
-#import xml.dom.minidom
-#s=http.client.HTTPConnection("openapi.gg.go.kr")
-#s.request("GET","/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1")
-#req=s.getresponse()
-#data=req.read()
-#dom=xml.dom.minidom.parseString(data)
-#print(req.getheaders())
-#print(dom.toxml())
+from urllib.parse import urlparse
+import http.client
+
+url="http://openapi.gg.go.kr/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1&pSize=50"
+parts = urlparse(url)
+parts.path
+
+conn = http.client.HTTPConnection("openapi.gg.go.kr")
+conn.request("GET", "/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1&pSize=50")
+req = conn.getresponse()
+print(req.status, req.reason)
+cLen = req.getheader("Content-Length")
+req.read(cLen) 
