7b4893702454a8452e21882846fbb92d98350485
정재훈 <jjh_2708@naver.com>
2016-05-27 13:14:52 +0900

    xml 파일 생성 코드 적용
    xml 파일 생성 코드 적용

--- Toilet Searcher/Toilet_Searcher.py
+++ Toilet Searcher/Toilet_Searcher.py
@@ -1,15 +1,70 @@
-#-*- encoding: utf-8 -*-
+#from xml.etree.ElementTree import parse
 
-from urllib.parse import urlparse
-import http.client
+#tree = parse("Toilet.xml")
+#note = tree.getroot()
+#print(note.get("date"))
+#print(note.get("foo", "default"))
+#print(note.keys())
+#print(note.items())
 
-url="http://openapi.gg.go.kr/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1&pSize=50"
-parts = urlparse(url)
-parts.path
+#import urllib.request
+#import xml.etree.ElementTree as etree
 
-conn = http.client.HTTPConnection("openapi.gg.go.kr")
-conn.request("GET", "/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1&pSize=50")
-req = conn.getresponse()
-print(req.status, req.reason)
-cLen = req.getheader("Content-Length")
-req.read(cLen) 
+#def main():
+
+#    ##서울공공데이터사용
+#    #key = 'yourKey'
+#    #url = "http://openAPI.seoul.go.kr:8088/%s/xml/SJWPerform/1/5" % key
+
+#    #data = urllib.request.urlopen(url).read()
+
+#    filename = "Toilet.xml"
+#    #f = open(filename, "wb") #다른 사람들의 예제처럼 "w"만 해서 했더니 에러가 발생
+#    #f.write(data)
+#    #f.close()
+
+#    #파싱하기
+#    tree = etree.parse(filename)
+#    root = tree.getroot()
+
+#    for a in root.findall('row'):
+#        print(a.findtext('DATA_STD_DE'))
+#        print(a.findtext('SIGUN_NM'))
+#        print(a.findtext('PUBLFACLT_DIV_NM'))
+#        print(a.findtext('PBCTLT_PLC_NM'))
+#        print(a.findtext('LOCPLC_ROADNM_ADDR'))
+#        print('----------------------')
+
+#if __name__ == "__main__":
+#    main()
+
+#-*- coding: utf-8 -*-
+
+
+import urllib.request
+import xml.etree.ElementTree as etree
+
+def main():
+
+    #서울공공데이터사용
+    key = 'yourKey'
+    url = "http://openapi.gg.go.kr/Publtolt?KEY=9bee44ffc03b4586a53534934f51af1c&Type=xml&pIndex=1&pSize=50"
+
+    data = urllib.request.urlopen(url).read()
+
+    filename = "sample1.xml"
+    f = open(filename, "wb") #다른 사람들의 예제처럼 "w"만 해서 했더니 에러가 발생
+    f.write(data)
+    f.close()
+
+    #파싱하기
+    tree = etree.parse(filename)
+    root = tree.getroot()
+
+    for a in root.findall('row'):
+        print(a.findtext('DATA_STD_DE'))
+        print(a.findtext('SIGUN_NM'))
+        print('----------------------')
+
+if __name__ == "__main__":
+    main()
---
+++ Toilet Searcher/main.py
@@ -0,0 +1,8 @@
+HOST = '127.0.0.1' 			#localhost
+PORT = 50007 			#서버와 같은 포트를 사용함
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #소켓 생성
+s.connect((HOST,PORT))
+s.send(b'Hello, python') 		#문자를 보냄
+data = s.recv(1024) 		#서버로 부터 정보를 받음
+s.close()
+print('Received', repr(data))
