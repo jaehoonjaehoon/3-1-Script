9fd9472df7f4629caba664e9b8fa7980a68b7a16
정재훈 <jjh_2708@naver.com>
2016-05-30 21:39:50 +0900

    코드 수정
    코드 수정

--- Toilet Searcher/Toilet_Searcher.py
+++ Toilet Searcher/Toilet_Searcher.py
@@ -42,15 +42,96 @@
 
 ##--------------------------------위는 구현된 부분
 
-# -*- coding: cp949 -*-
+# -*- coding: utf-8 -*-
 
-#from urllib.request
-#from urllib import urlencode, quote_plus
+import urllib.request,urllib.parse 
+from urllib.request import urlopen
+import http.client
+from urllib.parse import urlencode
+from urllib.parse import quote_plus
+from xml.etree import ElementTree
+class DaumOpenAPI:
+    #url�Է½� �� ���ĵ� 
+    server = 'api.suwon.go.kr'
+    regKey = 'q6mrRozl07bNXTjHeLqmLSvN4U%2FK7qvApXtiNo23QVTjFNjjIy2X8CE1YDV0snaOMTYKqjWQjdj1mi79PQLAKQ%3D%3D'
+    query = '스타벅스'                          #질의검색
+    location = '37.340226,126.733629'               #위도경도
+    radius= '20000'                         #반경 0~20000
+    count = '15'                          #페이지에 보여질 갯수1~15
+    page = '1'                            #페이지번호1~3
+    sort = '2'                            #0:정확도, 1:거리, 2:인기
+    format = "xml"                        #xml
+    andSign = '&'
+    conn = None
+    queryParams =None
+    baseUrl = 'http://api.suwon.go.kr/openapi-data/service/Toilet/getToilet?pageNo=1&numOfRows=10&ServiceKey='
+    xmlData = None
 
-#url = 'http://api.suwon.go.kr/openapi-data/service/Toilet/getToilet'
-#queryParams = '?' + urlencode({ quote_plus('ServiceKey') : '서비스키', quote_plus('numOfRows') : '999', quote_plus('pageNo') : '1' })
 
-#request = Request(url + queryParams)
-#request.get_method = lambda: 'GET'
-#response_body = urlopen(request).read()
-#print(response_body)
+    #def __init__(self):
+        #print('init')
+    def myURIBuilder(self):
+         self.queryParams = ('?' + urlencode({ quote_plus('query') : self.query,
+                               quote_plus('location') : self.location,
+                               quote_plus('radius') : self.radius,
+                               quote_plus('count') : self.count,
+                               quote_plus('sort') : self.sort,
+                               quote_plus('page') : self.page, 
+                               quote_plus('apikey'):self.regKey
+                                }))
+    #검색할 장소 xml형식으로 불러오기    
+
+    def getData(self): 
+        conn = http.client.HTTPConnection(self.server)
+        print('connection complate')
+
+        conn.request('GET',self.baseUrl+ self.queryParams )
+        print(self.baseUrl+ self.queryParams)
+        print('request complate\n')
+
+        req = conn.getresponse()
+        self.xmlData = req.read()
+        print(req.status,req.reason)
+
+        print(self.xmlData.decode('utf8'))
+
+    def updateQueryParam(self,key):
+
+        self.query = key
+        self.myURIBuilder()
+
+    def printInfo(self,rss):
+        for element in rss.findall("item"):
+           print('-------------------------------------------------------------------------------')
+
+           print('장소명:'+element.findtext('title'))
+
+           print('카테고리:'+element.findtext('category'))
+
+           print('전화번호:'+element.findtext('phone'))
+
+           print('구주소:'+element.findtext('address'))
+
+           print('도로명주소:'+element.findtext('newAddress'))
+
+           print('거리:'+element.findtext('distance')+'미터')
+
+           print('방향:'+element.findtext('direction'))
+           print('-------------------------------------------------------------------------------')
+    def extractTitleData(self):
+       rss = (ElementTree.parse(urlopen(self.baseUrl + self.queryParams))).getroot()
+       
+       self.printInfo(rss)
+    def getdataFromQuery(self,key):
+        self.updateQueryParam(key)
+        rss = (ElementTree.parse(urlopen(self.baseUrl + self.queryParams))).getroot()
+        #print(self.baseUrl + self.queryParams)
+        return rss
+    def getUrl(self,key):
+        self.updateQueryParam(key)
+        return self.baseUrl + self.queryParams
+if (__name__ == '__main__') :
+    a = DaumOpenAPI()
+        #print(type(a.count))
+    a.printInfo(a.getdataFromQuery('산기대'))
+        #a.getData()
